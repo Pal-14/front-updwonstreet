f2c6af1c0d7fa0c40e26db3c6d9f71278f9bb9e9
Robuchon Hugues <hugues.robuchon@gmail.com>
2021-11-02 12:21:12 +0100

    init projet

---
+++ src/services.js
@@ -0,0 +1,66 @@
+import axios from "axios";
+
+const baseURL = /* "https://glazo-back.osc-fr1.scalingo.io" || */ "http://192.168.1.10:5000";
+
+const base = axios.create({
+  baseURL,
+});
+const Service = {
+  
+};
+
+export default Service;
+// Rien à voir, circulez
+// Vraiment rien à voir, promis.
+// Bon ...
+if (process.env.NODE_ENV === "development") {
+  // Intercepte chaque requête du frontend pour faire un joli log
+  base.interceptors.request.use(
+    (request) => {
+      let { baseURL, method, url } = request;
+      console.log(
+        `SENDING [${method.toUpperCase()}] FROM FRONTEND : ${baseURL}${url}`
+      );
+      return request;
+    },
+    (err) => err
+  );
+
+  // Intercepte chaque réponse du backend pour faire un joli log
+  base.interceptors.response.use(
+    (response) => {
+      let { config, data, status, statusText } = response;
+      let { method } = config;
+      console.log("");
+      console.log("\\\\\\\\");
+      console.log(
+        `RECEIVED [${method.toUpperCase()}] RESPONSE FROM BACKEND : ${
+          config.baseURL
+        }${config.url}`
+      );
+      console.log(
+        `Data of type : ${typeof data} | status ${status} ${statusText}`
+      );
+      console.log("\\\\\\\\");
+      console.log("");
+      return response;
+    },
+    (err) => {
+      let { config, data, status, statusText } = err.response;
+      let { method } = config;
+      console.log("");
+      console.log("\\\\\\\\");
+      console.log(
+        `RECEIVED [${method.toUpperCase()}] RESPONSE FROM BACKEND : ${
+          config.baseURL
+        }${config.url}`
+      );
+      console.log(
+        `Data of type : ${typeof data} | status ${status} ${statusText}`
+      );
+      console.log("\\\\\\\\");
+      console.log("");
+      return err.response;
+    }
+  );
+}

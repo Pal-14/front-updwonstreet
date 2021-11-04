73ebd8d31a16d8bb288307476ca7e2e047531a7c
Virginie Fiorucci <vfiorucci@live.fr>
2021-11-04 10:44:19 +0100

    Admin.js - Mise en place de la recherche filtrée

--- src/View/Admin/Admin.js
+++ src/View/Admin/Admin.js
@@ -5,20 +5,34 @@ import Service from "../../services";
 
 function Admin() {
 
-    /* Déclaration variable d'état - Liste d'utilisateurs */
-    const [userList, setUserList] = useState([]);
+    /* Déclaration variables d'état - Liste d'utilisateurs */
+    const [userList, setUserList] = useState([]); // Tous les utilisateurs
+    const [filteredList, setFilteredList] = useState(userList); // Utilisateurs filtrés
+
+    /* Événement de recherche */
+    const handleSearch = (e) => {
+        let value = e.target.value.toLowerCase();
+        let result = [];
+        console.log("Value:", value);
+        result = userList.filter((data) => {
+            return (data.lastName.search(value) != -1) || (data.firstName.search(value) != -1);
+        });
+        setFilteredList(result);
+    };
 
     /* Récupération de tous les utilisateurs */
     useEffect(() => {
         Service.adminUserList().then((response) => {
             setUserList(response.data);
+            setFilteredList(response.data);
         });
     }, []);
-    console.log(userList);
+    console.log("User list:", userList);
+    console.log("Filtered list:", filteredList);
 
     /* Fonction d'affichage d'un utilisateur */
     const renderUsers = () => {
-        return userList.map((user, id) => {
+        return filteredList.map((user, id) => {
             return (
                 <div key={id} className="userIdentity">
                     <p>{user.firstName} {user.lastName}</p>
@@ -31,6 +45,7 @@ function Admin() {
     return (
         <div className="generalContainer">
             <h2>Liste des utilisateurs</h2>
+            <input type="text" placeholder="Rechercher" onChange={handleSearch} />
             <div className="userList">{renderUsers()}</div>
         </div>
     );
